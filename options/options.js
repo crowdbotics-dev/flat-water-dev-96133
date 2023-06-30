9d536222de541c809885eddb9556013e99bf89c0
Crowdbotics <engineering@crowdbotics.com>
2023-06-30 00:55:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "flat-water-dev-96133",
+  url: "https://flat-water-dev-96133.botics.co",
+  api: "https://flat-water-dev-96133.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
